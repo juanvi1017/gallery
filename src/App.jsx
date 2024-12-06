24c74600f495557e63a5a5b79e2e097e3c576701
jcaceres <jvcaceres17@gmail.com>
2024-12-06 17:05:57 -0500

    Fix: responsive gallery image

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import ListImage from './component/ListImage/ListImage';
 import MenuBottom from './component/MenuBottom/MenuBottom';
 import MenuTop from './component/MenuTop/MenuTop';
 import Hero from './component/Hero/Hero';
+import { Grid2 } from '@mui/material';
 
 function App() {
 
@@ -9,7 +10,11 @@ function App() {
     <>
       <MenuTop />
       <Hero />
-      <ListImage />
+      <Grid2 container spacing={1} justifyContent='center' textAlign='center'>
+        <Grid2 size={{ xs: 11, md: 9 }}>
+          <ListImage />
+        </Grid2>
+      </Grid2>
       <MenuBottom />
     </>
   )
