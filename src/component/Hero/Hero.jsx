72b44bfa3269441eb5669cf3e6b032244d9596c5
jcaceres <jvcaceres17@gmail.com>
2024-12-06 17:10:25 -0500

    text of hero

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -6,8 +6,8 @@ export default function Hero() {
     return (
         <div className='section-hero'>
             <h1>Inspirations</h1>
-            <p>Galeria de imagenes con scroll infinito, consumiendo api de unsplash, desarrollado en React Js, </p>
-            <p>con dependencias como infinityScroll.</p>
+            <p>Galeria de imagenes con scroll infinito, consumiendo api de unsplash, desarrollado en React Js,
+            con dependencias como infinityScroll.</p>
         </div>
     );
 }
