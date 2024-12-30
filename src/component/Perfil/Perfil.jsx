dd6358e9964f5717890770e8e6157476624f83c2
jcaceres <jvcaceres17@gmail.com>
2024-12-30 10:02:57 -0500

    Feature: perfil text

--- src/component/Perfil/Perfil.jsx
+++ src/component/Perfil/Perfil.jsx
@@ -14,17 +14,16 @@ export default function Perfil() {
             />
             <p>
                 Comencemos hablando de mi carrera. ¡Me encanta decir que soy ingeniero! soy uno de esos que se atrevío a estudiar una ingenería y para remate en Sistemas con enfasis en el desarrollo de software.
-                Mi deporte favorito es el fútbol, aunque mi equipo local a veces me haga enfurecer ¡Vamos rojo y blanco!; es una pasión que no se abandona.
-                En mis ratos libres, si no estoy viendo un partido, estoy aprendiendo sobre programación. Se podría decir que estos son mis dos vicios.
+                Mi deporte favorito es el fútbol, aunque mi equipo local a veces me haga enfurecer ¡Vamos rojo y blanco!.
+                En mis ratos libres, si no estoy disfrutando de un buen partido de fútbol, estoy aprendiendo sobre programación.
             </p>
 
             <p>
                 Me considero un joven apasionado por la tecnología, Ingeniero de Sistemas y fascinado por el desarrollo de software. Cuento con experiencia en diversos lenguajes,
-                frameworks y bibliotecas de programación. Mi objetivo es optimizar los procesos en las empresas, desarrollando software adaptativo, escalable e intuitivo para el usuario final.
-                Como desarrollador, he adquirido conocimientos y experiencia tanto en frontend como en backend, destacándome por mis sólidos conocimientos en Python, Django, JavaScript, React.js,
-                Material UI, Bootstrap, MySQL, Oracle, seguridad informática, APIs, Docker, y sistemas operativos Linux y Microsoft.
+                frameworks y bibliotecas de programación tanto en frontend como en backend, destacándome por mis sólidos conocimientos en JavaScript, React.js, Material UI, Bootstrap, Python, Django,
+                MySQL, Oracle, seguridad informática, APIs, Docker, y sistemas operativos Linux y Microsoft.
             </p>
-           <p className='parrafo'>Juan Caceres Miranda.</p>
+           <p className='parrafo'>Juan Cáceres Miranda.</p>
 
         </div>
     );
